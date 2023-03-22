78ea94021be395ad0359c5f57538cff83f499bd5
C25 <88030883+1838pm@users.noreply.github.com>
2023-03-22 15:09:22 +0900

    [Bronze V] Title: A+B, Time: 208 ms, Memory: 17680 KB -BaekjoonHub

--- 백준/Bronze/1000. A＋B/A＋B.java
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -1,13 +1,15 @@
 import java.util.Scanner;
-
+ 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        int A = scan.nextInt();
-        int B = scan.nextInt();
-
-        System.out.println(A+B);
-
-        scan.close();
-    }
+ 
+	public static void main(String[] args) {
+ 
+	Scanner sc = new Scanner(System.in);
+	int a = sc.nextInt();
+	int b = sc.nextInt();
+	
+	System.out.println(a + b);
+	
+	sc.close();
+	}
 }
